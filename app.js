d41c0367646c9ad52d2ce77974cc08432453ef6e
Eric Schwartz <eric@techtalentsouth.com>
2017-10-08 21:59:02 -0700

    added comments

--- app.js
+++ app.js
@@ -24,10 +24,16 @@ app.get('/get-user-info', (req, res) => {
 
 //route that processes data from the from and displays it on the screen
 app.get('/display-user-info', (req, res) => {
+
+  //lets create some variables that hold the data submitted from the form
+  //notice req.query.name. This is how we access the value of the "name" query parameter submitted by the form. 
+  //we repeat this pattern for the other query parameters we want to access (age and superhero).
   let name      = req.query.name;
   let age       = req.query.age;
   let superhero = req.query.superhero;
 
+  //here we tell the server to respond with the variables initilized above
+  //ultimateley displaying the data that was sent from the form
   res.send(`
     Your name is ${name}
     Your age is ${age}
